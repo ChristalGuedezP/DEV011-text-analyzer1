2f1fb5d207b4681a668a255fe5d8a2ff0295e0f8
Christal Guedez <141065544+christicki@users.noreply.github.com>
2023-09-04 13:41:17 -0300

    Update index.js
    arreglo con analyzer

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-//limpiar caja de texto// listoooo
-window.addEventListener('DOMContentLoaded', () => {
-    const textarea = document.querySelector('textarea');
-    textarea.addEventListener('keyup', () => {
-      // orden de todo con metricas(agregar analizer?//) no se muy bien como continuaaaaaaaaaaaaarrrrr////////
-      const analyzer = {  
-        getWordCount: (text) => {
-          //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-        },
-        getCharacterCount: (text) => {
-          //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-        },
-        getCharacterCountExcludingSpaces: (text) => {
-          //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-        },
-        getAverageWordLength: (text) => {    
-          //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-        },
-        getNumberCount: (text) => {
-          //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-        },
-        getNumberSum: (text) => {
-          //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-        },
-      };
+import analyzer from "./analyzer.js";
 
-    });
-  
-    const resetButton = document.getElementById('reset-button');
-  
-    resetButton.addEventListener('click', () => {
-      textarea.value = ''; 
-    });
-  });
+//limpiar caja de texto//listo ///
+const textarea = document.querySelector('#prueba');
+const resetButton = document.getElementById('reset-button');
+
+textarea.addEventListener('keyup', () => {
+  // orden de todo con metricas(agregar analizer?//)
+  analyzer.getWordCount(textarea.value)
+});
+resetButton.addEventListener('click', () => {
+  textarea.value = '';
+});  
+
