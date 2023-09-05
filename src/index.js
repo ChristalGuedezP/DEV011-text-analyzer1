717140f2e0a6d04da67fbdc8d1e4635c735808bd
Christal Guedez <141065544+christicki@users.noreply.github.com>
2023-09-04 22:32:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import analyzer from "./analyzer.js";
 
+//llamar elementos del DOM
+const words = document.querySelector('[data-testied="word-count"]')
+
 //limpiar caja de texto//listo ///
 const textarea = document.querySelector('#prueba');
 const resetButton = document.getElementById('reset-button');
 
+function actualizarMetricas() {
+
+///Conteo de Palabras
+const contarPalabras = analyzer.getWordCount(textarea.value);
+const wordCountSpan = document.getElementById("wordCount");
+console.log(contarPalabras);
+wordCountSpan.textContent = contarPalabras; 
+
+///Conteo de Caracteres
+
+
+}
+
 textarea.addEventListener('keyup', () => {
   // orden de todo con metricas(agregar analizer?//)
-  analyzer.getWordCount(textarea.value)
+  analyzer.getCharacterCount(textarea.value)
+  analyzer.getCharacterCountExcludingSpaces(textarea.value)
+  analyzer.getNumbersCount(textarea.value)
+  analyzer.getNumbersSum(textarea.value)
+  analyzer.getAverageWordLength(textarea.value)
 });
 resetButton.addEventListener('click', () => {
   textarea.value = '';
 });  
-
+ 
