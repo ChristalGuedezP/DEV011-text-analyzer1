af508b6d55d7721bc2399792e7ed90b7b5b3c574
Christal Guedez <141065544+christicki@users.noreply.github.com>
2023-09-04 23:33:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
 import analyzer from "./analyzer.js";
 
-//llamar elementos del DOM
-const words = document.querySelector('[data-testied="word-count"]')
+// Llamar elementos del DOM
+const words = document.querySelector('[data-testied="word-count"]');
 
-//limpiar caja de texto//listo ///
+// Limpiar caja de texto
 const textarea = document.querySelector('#prueba');
 const resetButton = document.getElementById('reset-button');
 
-function actualizarMetricas() {
-
-///Conteo de Palabras
-const contarPalabras = analyzer.getWordCount(textarea.value);
-const wordCountSpan = document.getElementById("wordCount");
-console.log(contarPalabras);
-wordCountSpan.textContent = contarPalabras; 
-
-///Conteo de Caracteres
-
-
-}
-
+// Funciones
 textarea.addEventListener('keyup', () => {
-  // orden de todo con metricas(agregar analizer?//)
-  analyzer.getCharacterCount(textarea.value)
-  analyzer.getCharacterCountExcludingSpaces(textarea.value)
-  analyzer.getNumbersCount(textarea.value)
-  analyzer.getNumbersSum(textarea.value)
-  analyzer.getAverageWordLength(textarea.value)
+  //Conteo de Palabras
+  const contarPalabras = analyzer.getWordCount(textarea.value);
+  const wordCountSpan = document.getElementById('wordCount');
+  wordCountSpan.textContent = contarPalabras; 
+
+  //Conteo de Caracteres
+  const contarCaracteres = analyzer.getCharacterCount(textarea.value);
+  const characterCountSpan = document.getElementById('characterCount');
+  characterCountSpan.textContent = contarCaracteres; 
+
+  //Conteo de Caracteres sin espacios ni puntuación
+  const contarCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  const characterNoSpacesCountSpan = document.getElementById('characterNoSpacesCount');
+  characterNoSpacesCountSpan.textContent = contarCaracteresSinEspacios; 
+
+  //Conteo de Números
+  const contarNumeros = analyzer.getNumbersCount(textarea.value);
+  const numberCountSpan = document.getElementById('numberCount');
+  numberCountSpan.textContent = contarNumeros; 
+
+  //Suma de Números
+  const sumadeNumeros = analyzer.getNumbersSum(textarea.value);
+  const numbersSumSpan = document.getElementById('numbersSum');
+  numbersSumSpan.textContent = sumadeNumeros; 
+
+  //Longitud Media de Palabras
+  const longituddePalabras = analyzer.getAverageWordLength(textarea.value);
+  const wordLengthAverageSpan = document.getElementById('wordLengthAverage');
+  wordLengthAverageSpan.textContent = longituddePalabras;
 });
+
 resetButton.addEventListener('click', () => {
   textarea.value = '';
-});  
- 
+});
