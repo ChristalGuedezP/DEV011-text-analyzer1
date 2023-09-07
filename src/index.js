1cb4399a30632031f9ecc06df2b0bfaf00ac0397
Christal Guedez <141065544+christicki@users.noreply.github.com>
2023-09-07 19:53:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
 import analyzer from "./analyzer.js";
 
-// Llamar elementos del DOM
-const words = document.querySelector('[data-testid="word-count"]');
-
-
 // Limpiar caja de texto
-const textarea = document.querySelector('#prueba');
-const resetButton = document.getElementById('reset-button');
+const textarea = document.querySelector('textarea[name="user-input"]');
+const resetButton = document.querySelector("#reset-button");
 
 // Funciones
-textarea.addEventListener('keyup', () => {
+textarea.addEventListener("keyup", () => {
   //Conteo de Palabras
   const contarPalabras = analyzer.getWordCount(textarea.value);
-  const wordCountSpan = document.getElementById('wordCount');
-  wordCountSpan.textContent = contarPalabras; 
+  const wordCountLi = document.querySelector('[data-testid="word-count"]');
+  wordCountLi.textContent = `Recuento de palabras: ${contarPalabras}`;
 
-  //Conteo de Caracteres
-  const contarCaracteres = analyzer.getCharacterCount(textarea.value);
-  const characterCountSpan = document.getElementById('characterCount');
-  characterCountSpan.textContent = contarCaracteres; 
+  // Conteo de Caracteres
+  let contarCaracteres = analyzer.getCharacterCount(textarea.value);
+  const characterCountLi = document.querySelector(
+    '[data-testid="character-count"]'
+  );
+  characterCountLi.textContent = `Recuento de caracteres: ${contarCaracteres}`;
 
   //Conteo de Caracteres sin espacios ni puntuación
-  const contarCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(textarea.value);
-  const characterNoSpacesCountSpan = document.getElementById('characterNoSpacesCount');
-  characterNoSpacesCountSpan.textContent = contarCaracteresSinEspacios; 
+  const contarCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(
+    textarea.value
+  );
+  const characterNoSpacesCountLi = document.querySelector(
+    '[data-testid="character-no-spaces-count"]'
+  );
+  characterNoSpacesCountLi.textContent = `Recuento de caracteres excluyendo espacios y signos de puntuación: ${contarCaracteresSinEspacios}`;
 
   //Conteo de Números
-  const contarNumeros = analyzer.getNumbersCount(textarea.value);
-  const numberCountSpan = document.getElementById('numberCount');
-  numberCountSpan.textContent = contarNumeros; 
+  const contarNumeros = analyzer.getNumberCount(textarea.value);
+  const numberCountLi = document.querySelector('[data-testid="number-count"]');
+  numberCountLi.textContent = `Recuento de números: ${contarNumeros}`;
 
   //Suma de Números
-  const sumadeNumeros = analyzer.getNumbersSum(textarea.value);
-  const numbersSumSpan = document.getElementById('numbersSum');
-  numbersSumSpan.textContent = sumadeNumeros; 
+  const sumadeNumeros = analyzer.getNumberSum(textarea.value);
+  const numbersSumLi = document.querySelector('[data-testid="numbers-sum"]');
+  numbersSumLi.textContent = `Suma de números: ${sumadeNumeros}`;
 
   //Longitud Media de Palabras
   const longituddePalabras = analyzer.getAverageWordLength(textarea.value);
-  const wordLengthAverageSpan = document.getElementById('wordLengthAverage');
-  wordLengthAverageSpan.textContent = longituddePalabras;
+  const wordLengthAverageLi = document.querySelector(
+    '[data-testid="word-length-average"]'
+  );
+  wordLengthAverageLi.textContent = `Longitud media de palabras: ${longituddePalabras}`;
 });
 
-resetButton.addEventListener('click', () => {
-  textarea.value = '';
+resetButton.addEventListener("click", () => {
+  textarea.value = "";
+
+  // Llamar elementos del Dom
+  const wordCountElement = document.querySelector('[data-testid="word-count"]');
+  const characterCountElement = document.querySelector(
+    '[data-testid="character-count"]'
+  );
+  const characterNoSpacesCountElement = document.querySelector(
+    '[data-testid="character-no-spaces-count"]'
+  );
+  const numberCountElement = document.querySelector(
+    '[data-testid="number-count"]'
+  );
+  const numbersSumElement = document.querySelector(
+    '[data-testid="numbers-sum"]'
+  );
+  const wordLengthAverageElement = document.querySelector(
+    '[data-testid="word-length-average"]'
+  );
+  //Limpiar metricas
+  wordCountElement.textContent = "Recuento de palabras: 0";
+  characterCountElement.textContent = "Recuento de caracteres: 0";
+  characterNoSpacesCountElement.textContent =
+    "Recuento de caracteres excluyendo espacios y signos de puntuación: 0";
+  numberCountElement.textContent = "Recuento de números: 0";
+  numbersSumElement.textContent = "Suma de números: 0";
+  wordLengthAverageElement.textContent = "Longitud media de palabras: 0";
 });
+
