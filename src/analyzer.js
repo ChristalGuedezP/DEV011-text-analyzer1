48c3720a9ac6b21e3b2ab3f94e11830d8ec2e6ef
Christal Guedez <141065544+christicki@users.noreply.github.com>
2023-09-07 19:52:44 -0300

    Update analyzer.js
    errores acomodados

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,5 +1,5 @@
-const analyzer = {  
-////CONTAR PALABRAS///
+const analyzer = {
+  // Contar palabras
   getWordCount: (text) => {
     text = text.trim();
     const words = text.split(/\s+/);
@@ -7,60 +7,60 @@ const analyzer = {
     return words.length;
   },
 
-///CONTAR CARACTERES///
+  // Contar caracteres
   getCharacterCount: (text) => {
-    text = text.trim();
-    const characterCount = text.length;
+    let characterCount = 0;
+    for (let i = 0; i < text.length; i++) {
+      characterCount++;
+    }
     console.log(`Recuento de caracteres: ${characterCount}`);
-        return characterCount;
+    return characterCount;
   },
-
-///CONTAR CARACTERES EXCLUYENDO ESPACIOS Y PUNTUACION///
-getCharacterCountExcludingSpaces: (text) => {
-  const cleanText = text.replace(/[^\w]/g, "");
-  const characterCountExcludingSpaces = cleanText.length;
-  console.log(`Recuento de caracteres excluyendo espacios y signos de puntuación: ${characterCountExcludingSpaces}`);
-  return characterCountExcludingSpaces;
-},
-
-///CONTEO DE NUMEROS///  
-  getNumbersCount: (text) => {
+  ///Contar Caracteres sin incluir Espacios ni Puntuacion
+  getCharacterCountExcludingSpaces: (text) => {
+    const cleanText = text.replace(/[^\w]/g, "");
+    const characterCountExcludingSpaces = cleanText.length;
+    console.log(
+      `Recuento de caracteres excluyendo espacios y signos de puntuación: ${characterCountExcludingSpaces}`
+    );
+    return characterCountExcludingSpaces;
+  },
+  //Conteo de numeros
+  getNumberCount: (text) => {
     const numbers = text.match(/\d+/g);
     if (!numbers) {
-      console.log('Recuento de números: 0');
+      console.log("Recuento de números: 0");
       return 0;
     }
     console.log(`Recuento de números: ${numbers.length}`);
     return numbers.length;
   },
 
-///SUMA DE NUMEROS///  
-getNumbersSum: (text) => {
-  const numbers = text.match(/\d+/g);
+  //Suma de Numeros
+  getNumberSum: (text) => {
+    const numbers = text.match(/\d+/g);
 
-  if (!numbers) {
-    console.log('Suma de los números: 0');
-    return 0;
-  }
+    if (!numbers) {
+      console.log("Suma de los números: 0");
+      return 0;
+    }
 
-  const sum = numbers.reduce((total, number) => total + parseInt(number, 10), 0);
-  console.log(`Suma de los números: ${sum}`);
-
-  return sum;
+    const sum = numbers.reduce(
+      (total, number) => total + parseInt(number, 10),
+      0
+    );
+    console.log(`Suma de los números: ${sum}`);
+    return sum;
   },
 
-///LONGITUD MEDIA DE PALABRAS///
+  // Calcular la longitud media de las palabras
   getAverageWordLength: (text) => {
     const words = text.split(/\s+/);
-
     const totalLength = words.reduce((sum, word) => sum + word.length, 0);
-
     const averageLength = totalLength / words.length;
     console.log(`Longitud media de palabras: ${averageLength}`);
-
-    return averageLength;
+    return parseFloat(averageLength.toFixed(2));
   },
 };
 
 export default analyzer;
-
